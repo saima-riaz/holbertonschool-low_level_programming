8776e867d1311a1017ce9a95f56cbf1db841acab
saima-riaz <saimariaz1422@gmail.com>
2023-06-21 08:47:23 -0700

    task 3. islower

--- functions_nested_loops/3-main.c
+++ functions_nested_loops/3-main.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- *main - check the code.
- *
- *Return: Always 0.
- **/
-	int main(void)
+* test_islower - Test the _islower function
+*
+* @n: Number to pass to _islower function
+*/
+void test_islower(int n)
 {
-	int r;
-	r = _islower(n);
-	_putchar(r + '0');
-	_putchar('\n');
-	 return (0);
+int r;
+
+ r = _islower(n);
+ _putchar(r + '0');
+ _putchar('\n');
 }
