041ca8fd43ed74f0b1fc07a14cead18f48ac5539
saima-riaz <saimariaz1422@gmail.com>
2023-06-21 08:53:15 -0700

    task 3. islower

--- functions_nested_loops/3-main.c
+++ functions_nested_loops/3-main.c
@@ -1,15 +1,21 @@
-#include "main.h"
+julien@ubuntu:~/0x02$ cat 3-main.c
+#include "holberton.h"
 
-/**
-* test_islower - Test the _islower function
-*
-* @n: Number to pass to _islower function
-*/
-void test_islower(int n)
+		/**
+		 * main - check the code for ALX School students.
+		 *
+		 * Return: Always 0.
+		 */
+int main(void)
 {
 int r;
 
- r = _islower(n);
+ r = _islower('H');
+ _putchar(r + '0');
+ r = _islower('o');
+ _putchar(r + '0');
+ r = _islower(108);
  _putchar(r + '0');
  _putchar('\n');
+ return (0);
 }
