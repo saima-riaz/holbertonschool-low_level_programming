bccd3bd86dd7af4235aa879baf3545fff1ace748
saima-riaz <saimariaz1422@gmail.com>
2023-06-21 04:39:05 -0700

    task 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *print_alphabet 
+ * 
+ *Return: void
+ */
+void print_alphabet(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	
+	_putchar (alphbet);
+	_putchar('\n');
+}
