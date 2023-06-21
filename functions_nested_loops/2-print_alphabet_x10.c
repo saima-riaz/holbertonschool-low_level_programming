09e97e1f40c487217dfe7b970c9a00afada2f76c
saima-riaz <saimariaz1422@gmail.com>
2023-06-21 06:58:23 -0700

    task 2. 10 x alphabet

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+*prints_x10 -print 10 times the alphabet, in lowercase, followed by a new line.
+*
+*/
+
+	void print_alphabet_x10(void)
+
+{
+	int count = 0;
+	char alphabet;
+
+	while (count++ <= 9)
+
+{
+
+	for (alphabet = 'a'; alphabet <= 'z'; aphahbet++)
+
+	_putchar(alphabet);
+
+}
+
+	_putchar('\n');
+
+}
