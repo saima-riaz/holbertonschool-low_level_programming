e5d54457c51585c9edf9f8010066d1cfc7bab7bd
saima-riaz <saimariaz1422@gmail.com>
2023-06-23 08:32:02 -0700

    task 0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h
+
+/**
+* function that checks for uppercase character
+*
+*return 1 or 0
+*/
+
+int _isupper(int c);
+
+{
+if (c >= 'A' c <= 'Z')
+return (1);
+else
+return (0);
+}
