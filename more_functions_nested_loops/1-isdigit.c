17a9d1dd264887bcab9d476644ba231078ebbad1
saima-riaz <saimariaz1422@gmail.com>
2023-06-23 09:12:44 -0700

    task 1

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,10 +1,12 @@
 #include "main.h"
 
 /**
- *_isdigit - check for digit
- *
- *return 1 or 0
+ * _isdigit - checks for digit
+ * @c: check value
+ * Return: (1) if digit
  */
+
+
 int _isdigit(int c)
 {
 if (c >= 48 && c <= 57)
