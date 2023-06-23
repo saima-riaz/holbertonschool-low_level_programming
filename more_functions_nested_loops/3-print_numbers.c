53b2e512c6c1dee681716df32adc1b6868e1d82e
saima-riaz <saimariaz1422@gmail.com>
2023-06-23 10:01:42 -0700

    task 3

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -7,14 +7,14 @@
  * Return: (void)
  */
 
+
 void print_numbers(void)
 {
-int x;
+  char c;
 
-for (x = 0; x <= 9; x++)
-{
-_putchar('0');
-}
-
- _putchar('\n');
+  for (c = '0'; c <= '9'; c++)
+    {
+      _putchar (c);
+    }
+  _putchar ('\n');
 }
