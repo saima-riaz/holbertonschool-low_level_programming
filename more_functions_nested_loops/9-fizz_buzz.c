240c77e8f59259990a072dc5605d54d9879fcaba
saima-riaz <saimariaz1422@gmail.com>
2023-06-23 17:27:06 -0700

    task 9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -7,24 +7,24 @@
  */
 int main(void)
 {
-  int i;
+int i;
 
-  for (i = 1; i <= 100; i++)
-    {
-      if (i % 3 == 0 || i % 5 == 0)
-	{
-	  if (i % 3 == 0)
-	    {
-	      printf("Fizz");
-	    }
-	  if (i % 5 == 0)
-	    {
-	      printf("Buzz");
-	    }
-	} else
-	{
-	  printf("%d", i);
-	}
+for (i = 1; i <= 100; i++)
+{
+if (i % 3 == 0 || i % 5 == 0)
+{
+if (i % 3 == 0)
+{
+printf("Fizz");
+}
+if (i % 5 == 0)
+{
+printf("Buzz");
+}
+else
+{
+printf("%d", i);
+}
 if (i != 100)
 printf(" ");
 }
