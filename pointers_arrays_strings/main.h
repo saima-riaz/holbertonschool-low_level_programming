ac19f5ad34cca4d1639167bec83789944f123d45
saima-riaz <saimariaz1422@gmail.com>
2023-06-28 05:11:37 -0700

    task 0

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,3 +1,7 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -10,3 +14,6 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+char *_strcat(char *dest, char *src);
+
+#endif
