1734c5103812f1d70bb5053f65d270f1d0f5dcc9
saima-riaz <saimariaz1422@gmail.com>
2023-07-05 07:15:03 -0700

    general

---
+++ static_libraries/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for digit
+ * @c: check value
+ * Return: (1) if digit
+ */
+
+
+int _isdigit(int c)
+{
+if (c >= 48 && c <= 57)
+{
+return (1);
+}
+return (0);
+}
