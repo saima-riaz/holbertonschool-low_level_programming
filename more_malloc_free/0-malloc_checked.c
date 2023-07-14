1adcf7c174ea4a6bce447581d84cde7aa11774f1
saima-riaz <saimariaz1422@gmail.com>
2023-07-14 06:25:40 -0700

    task 0-malloc_checked.c

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,11 +1,12 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- *malloc_checked - allocate memory using malloc.
- *Description: If malloc fails, terminate process with status 98.
- *@b: unsigned int memory size to allocate
- *Return: void pointer to malloc'd memory space
- **/
+ * malloc_checked - allocate memory using malloc.
+ * Description: If malloc fails, terminate process with status 98.
+ * @b: unsigned int memory size to allocate
+ * Return: void pointer to malloc'd memory space
+ */
+
 void *malloc_checked(unsigned int b)
 {
 void *p;
