136f0ab3483bf4632258cd1ca6168733960fcd17
saima-riaz <saimariaz1422@gmail.com>
2023-07-21 13:55:35 -0700

    3-print_all.c

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -4,7 +4,23 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+
+/**
+ * struct op - Struct op
+ *
+ * @op: format
+ * @f: The function associated
+ */
+typedef struct op
+{
+	char *op;
+	void (*f)(va_list ap);
+} op_t;
+
+
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 #endif
