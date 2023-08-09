6960671c50a61b34430abf26ca3867abd838abd8
saima-riaz <saimariaz1422@gmail.com>
2023-08-09 03:42:55 -0700

    1-create_file.c

---
+++ file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * create_file - creat new file
+ * @filename : name of file
+ * @text_content: text inside new file
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int i = 0;
+	int wr;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+	while (text_content[i])
+		i++;
+	wr = write(fd, text_content, i);
+		if (wr == -1)
+		{	close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
+}
