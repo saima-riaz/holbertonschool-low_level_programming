b54ded00a1bb677d2738207b2f022d4a3cfa703f
saima-riaz <saimariaz1422@gmail.com>
2023-09-21 05:47:55 -0700

    changed decription

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -5,8 +5,7 @@
  * @array: pointer to the first element of the array
  * @size: is the number of elements in array
  * @value: is the value to search for
- * Description: Function that searches for a value in an
- * array of integers using the Linear search algorithm.
+ * 
  * Return: If value not present or if array is NULL return -1,
  * else return first index where value is located.
  */
